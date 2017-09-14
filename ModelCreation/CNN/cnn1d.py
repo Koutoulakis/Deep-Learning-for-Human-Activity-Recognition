9d666c1bda63664ffea33a3ac29234641c9e74b3
Ioannis Koutoulakis <koutoulakis_ioannis@yahoo.gr>
2017-09-15 00:47:40 +0100

    Add files via upload

--- ModelCreation/CNN/cnn1d.py
+++ ModelCreation/CNN/cnn1d.py
@@ -8,7 +8,7 @@ from sklearn import metrics
 import h5py
 import os
 import sys
-
+import matplotlib.pyplot as plt
 #%matplotlib inline
 plt.style.use('ggplot')
 
@@ -20,6 +20,13 @@ plt.style.use('ggplot')
 #     sigma = np.std(dataset,axis = 0)
 #     return (dataset - mu)/sigma
     
+def variable_summaries(var,name):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+  with tf.name_scope('summaries_'+name):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean_'+name, mean)
+    tf.summary.scalar(name+'_value',var)
+    tf.summary.histogram('histogram_'+name, var)
 
 def windowz(data, size):
     start = 0
@@ -75,6 +82,21 @@ def segment_pa2(x_train,y_train,window_size):
             i_segment+=1
     return segments, labels
 
+def segment_sph(x_train,y_train,window_size):
+    segments = np.zeros(((len(x_train)//(window_size//2))-1,window_size,52))
+    labels = np.zeros(((len(y_train)//(window_size//2))-1))
+    i_segment = 0
+    i_label = 0
+    for (start,end) in windowz(x_train,window_size):
+        if(len(x_train[start:end]) == window_size):
+            m = stats.mode(y_train[start:end])
+            segments[i_segment] = x_train[start:end]
+            labels[i_label] = m[0]
+            i_label+=1
+            i_segment+=1
+    return segments, labels
+
+
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev = 0.1)
     return tf.Variable(initial)
@@ -124,7 +146,7 @@ elif dataset =="dap":
     path = os.path.join(os.path.expanduser('~'), 'Downloads', 'dataset_fog_release','dataset_fog_release', 'daphnet.h5')
 elif dataset =="pa2":
     path = os.path.join(os.path.expanduser('~'), 'Downloads', 'PAMAP2_Dataset', 'pamap2.h5')
-elif dataset =="sphere":
+elif dataset =="sph":
     path = os.path.join(os.path.expanduser('~'), 'Downloads', 'SphereDataset', 'sphere.h5')
 else:
     print "Dataset not supported yet"
@@ -194,7 +216,11 @@ elif dataset =="pa2":
     test_x, test_y = segment_pa2(x_test,y_test,input_width)
     print "signal segmented."
 elif dataset =="sph":
-    print "sph seg"
+    input_width = 25
+    print "segmenting signal..."
+    train_x, train_y = segment_sph(x_train,y_train,input_width)
+    test_x, test_y = segment_sph(x_test,y_test,input_width)
+    print "signal segmented."
 else:
     print "no correct dataset"
 
@@ -252,15 +278,15 @@ elif dataset =="sph":
     print "sph"
     input_height = 1
     input_width = input_width #or 90 for actitracker
-    num_labels = 18  #or 6 for actitracker
-    num_channels = 77 #or 3 for actitracker
+    num_labels = 20  #or 6 for actitracker
+    num_channels = 52 #or 3 for actitracker
 else:
     print "wrong dataset"
 batch_size = 64
 stride_size = 2
 kernel_size_1 = 7
 kernel_size_2 = 3
-kernel_size_3 = 3
+kernel_size_3 = 1
 depth_1 = 128
 depth_2 = 128
 depth_3 = 128
@@ -303,6 +329,11 @@ h_conv1 = tf.nn.dropout(h_conv1, dropout_1)
 
 h_pool1 = max_pool(h_conv1,kernel_size_1,stride_size)
 
+# variable_summaries(W_conv1)
+# variable_summaries(b_conv1)
+# tf.summary.histogram('h_pool1', h_pool1)
+# tf.summary.histogram('h_pool1/sparsity', tf.nn.zero_fraction(h_pool1))
+
 print "hidden layer 1 shape"
 print "W_conv1 shape =",W_conv1.get_shape()
 print "b_conv1 shape =",b_conv1.get_shape()
@@ -318,6 +349,12 @@ h_conv2 = tf.nn.relu(depth_conv2d(h_pool1, W_conv2) + b_conv2)
 h_conv2 = tf.nn.dropout(h_conv2, dropout_2)
 
 h_pool2 = max_pool(h_conv2,kernel_size_2,stride_size)
+
+# variable_summaries(W_conv2)
+# variable_summaries(b_conv2)
+# tf.summary.histogram('h_pool2', h_pool2)
+# tf.summary.histogram('h_pool2/sparsity', tf.nn.zero_fraction(h_pool2))
+
 
 print "hidden layer 2 shape"
 print "W_conv2 shape =",W_conv2.get_shape()
@@ -358,6 +395,11 @@ print "c_flat shape =",h_pool3_flat.shape
 h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat,W_fc1) + b_fc1)
 h_fc1 = tf.nn.dropout(h_fc1, dropout_3)
 
+# variable_summaries(W_fc1)
+# variable_summaries(b_fc1)
+# tf.summary.histogram('h_fc1', h_fc1)
+# tf.summary.histogram('h_fc1/sparsity', tf.nn.zero_fraction(h_fc1))
+
 #readout layer.
 
 W_fc2 = weight_variable([num_hidden,num_labels])
@@ -365,23 +407,35 @@ b_fc2 = bias_variable([num_labels])
 
 y_conv = tf.matmul(h_fc1,W_fc2) + b_fc2
 
+# variable_summaries(W_fc2)
+# variable_summaries(b_fc2)
+# tf.summary.histogram('y_conv', y_conv)
+# tf.summary.histogram('y_conv/sparsity', tf.nn.zero_fraction(y_conv))
+
 
 # cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y_conv))
 # train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 
 # COST FUNCTION
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y_conv))#-tf.reduce_sum(Y * tf.log(y_conv))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate).minimize(loss)
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate).minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(loss)
 
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(Y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+# variable_summaries(loss,'loss')
+# variable_summaries(loss)
+# global_step = tf.Variable(0, dtype=tf.int32, trainable=False)
 
 # TRAINING THE MODEL
-
+loss_over_time = np.zeros(training_epochs)
 with tf.Session() as session:
 
+    # merged_summary_op = tf.summary.merge_all()
+    # summary_writer = tf.summary.FileWriter("./", session.graph)
     tf.initialize_all_variables().run()
+
     for epoch in range(training_epochs):
 
         cost_history = np.empty(shape=[0],dtype=float)
@@ -392,33 +446,57 @@ with tf.Session() as session:
             
             # print "batch_x shape =",batch_x.shape
             # print "batch_y shape =",batch_y.shape
-
-            _, c = session.run([optimizer, loss],feed_dict={X: batch_x, Y : batch_y, dropout_1: 1-0.1, dropout_2: 1-0.25, dropout_3: 1-0.5})
+            # _, c, summary= session.run([optimizer, loss,merged_summary_op],feed_dict={X: batch_x, Y : batch_y, dropout_1: 1-0.1, dropout_2: 1-0.25, dropout_3: 1-0.5})
+            # cost_history = np.append(cost_history,c)
+            _, c= session.run([optimizer, loss],feed_dict={X: batch_x, Y : batch_y, dropout_1: 1-0.1, dropout_2: 1-0.25, dropout_3: 1-0.5})
             cost_history = np.append(cost_history,c)
+            # summary_writer.add_summary(summary,global_step.eval(session=session))
         print "Epoch: ",epoch," Training Loss: ",np.mean(cost_history)," Training Accuracy: ",session.run(accuracy, feed_dict={X: train_x, Y: train_y, dropout_1: 1-0.1, dropout_2: 1-0.25, dropout_3: 1-0.5})
+        loss_over_time[epoch] = np.mean(cost_history)
     
     print "Testing Accuracy:", session.run(accuracy, feed_dict={X: test_x, Y: test_y, dropout_1: 1, dropout_2: 1, dropout_3: 1})
     y_p = tf.argmax(y_conv, 1)
     val_accuracy, y_pred = session.run([accuracy, y_p], feed_dict={X:test_x, Y:test_y, dropout_1: 1, dropout_2: 1, dropout_3: 1})
     print "validation accuracy:", val_accuracy
     y_true = np.argmax(test_y,1)
-    print "Precision,micro", metrics.precision_score(y_true, y_pred,average="micro")
-    print "Precision,macro", metrics.precision_score(y_true, y_pred,average="macro")
-    print "Precision,weighted", metrics.precision_score(y_true, y_pred,average="weighted")
-    #print "Precision,samples", metrics.precision_score(y_true, y_pred,average="samples")
-    print "Recall_micro", metrics.recall_score(y_true, y_pred, average="micro")
-    print "Recall_macro", metrics.recall_score(y_true, y_pred, average="macro")
-    print "Recall_weighted", metrics.recall_score(y_true, y_pred, average="weighted")
+    # print "Precision,micro", metrics.precision_score(y_true, y_pred,average="micro")
+    # print "Precision,macro", metrics.precision_score(y_true, y_pred,average="macro")
+    # print "Precision,weighted", metrics.precision_score(y_true, y_pred,average="weighted")
+    # #print "Precision,samples", metrics.precision_score(y_true, y_pred,average="samples")
+    # print "Recall_micro", metrics.recall_score(y_true, y_pred, average="micro")
+    # print "Recall_macro", metrics.recall_score(y_true, y_pred, average="macro")
+    # print "Recall_weighted", metrics.recall_score(y_true, y_pred, average="weighted")
     #print "Recall_samples", metrics.recall_score(y_true, y_pred, average="samples")
-    print "f1_score_micro", metrics.f1_score(y_true, y_pred, average="micro")
-    print "f1_score_macro", metrics.f1_score(y_true, y_pred, average="macro")
-    print "f1_score_weighted", metrics.f1_score(y_true, y_pred, average="weighted")
-    if dataset=="dap":
-        print "f1_score_", metrics.f1_score(y_true, y_pred)
+    if dataset=="opp" or dataset == "pa2" :
+        #print "f1_score_mean", metrics.f1_score(y_true, y_pred, average="micro")
+        print "f1_score_w", metrics.f1_score(y_true, y_pred, average="weighted")
+        
+        print "f1_score_m", metrics.f1_score(y_true, y_pred, average="macro")
+        # print "f1_score_per_class", metrics.f1_score(y_true, y_pred, average=None)
+    elif dataset=="dap":
+        print "f1_score_m", metrics.f1_score(y_true, y_pred, average="macro")
+    elif dataset=="sph":
+        print "f1_score_mean", metrics.f1_score(y_true, y_pred, average="micro")
+        print "f1_score_w", metrics.f1_score(y_true, y_pred, average="weighted")
+        
+        print "f1_score_m", metrics.f1_score(y_true, y_pred, average="macro")
+    else:
+        print "wrong dataset"
+    # print "f1_score_micro", metrics.f1_score(y_true, y_pred, average="micro")
+    # print "f1_score_macro", metrics.f1_score(y_true, y_pred, average="macro")
+    # print "f1_score_weighted", metrics.f1_score(y_true, y_pred, average="weighted")
+    # if dataset=="dap":
+    #     print "f1_score_", metrics.f1_score(y_true, y_pred)
     #print "f1_score_samples", metrics.f1_score(y_true, y_pred, average="samples")
     print "confusion_matrix"
     print metrics.confusion_matrix(y_true, y_pred)
     #fpr, tpr, tresholds = metrics.roc_curve(y_true, y_pred)
+    # plt.figure(1)
+    # plt.plot(loss_over_time)
+    # plt.title("Loss value over epochs (CNN DG)")
+    # plt.xlabel("Epoch")
+    # plt.ylabel("Loss")
+    # plt.show()
 
 
     #######################################################################################
